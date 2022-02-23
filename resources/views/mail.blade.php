092012e739286023e289d6604b981653fca767ec
konarch-dev <konarch92@gmail.com>
2022-02-23 17:55:43 +0530

    first commit

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -19,23 +19,21 @@
 									<div class="col-md-4 mail-grid">
 										<i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>
 										<h5>Address</h5>
-										<p>9th floor - Palace Building - 221B Walk of Fame - USA</p>
+										<p>vadodara, gujarat</p>
 									</div>
 									<div class="col-md-4 mail-grid">
 										<i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>
 										<h5>Phone</h5>
-										<p>Telephone:  +1 800 603 6035</p>
+										<p>Telephone:  +48 739517137</p>
 									</div>
 									<div class="col-md-4 mail-grid">
 										<i class="glyphicon glyphicon-envelope" aria-hidden="true"></i>
 										<h5>E-mail</h5>
-										<p>E-mail:<a href="mailto:example@mail.com"> example@mail.com</a></p>
+										<p>E-mail:<a href="mailto:konarchpl@gmail.com"> example@mail.com</a></p>
 									</div>
 									<div class="clearfix"></div>
 								</div>
-								<div class="map-w3">
-								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d673607.6340751307!2d-104.44001811743372!3d48.738351336759585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5321f600f5170943%3A0x94f2e8e71e1dfc7a!2sE+Comertown+Rd%2C+Westby%2C+MT+59275%2C+USA!5e0!3m2!1sen!2sin!4v1467080368135"  allowfullscreen></iframe>
-								</div>
+								
 								<div class="mail-bottom">
 									<h4>Get In Touch With Us</h4>
 									<form action="#" method="post">
